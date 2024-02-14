040b708a17247f6d6a7d0ad3c2402fd0c985aa20
Guillem <guillemruizchenoll@gmail.com>
2024-02-14 17:22:57 +0100

    Implementado los controladores

--- src/main/java/org/example/actividadfinal/DispositivoController.java
+++ src/main/java/org/example/actividadfinal/DispositivoController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import org.example.actividadfinal.models.Dispositivo;
 
 public class DispositivoController {
 
@@ -27,14 +28,68 @@ public class DispositivoController {
     @FXML
     private TextField tfPrecio;
 
-    @FXML
-    void onAltaModificacionClick(ActionEvent event) {
+    private InventarioController inventarioController;
+
+    public DispositivoController() {
 
     }
 
     @FXML
-    void initialize() {
+    public void initialize() {
         cbTipoDispositivo.getItems().setAll(TipoAtributo.values());
     }
+
+    @FXML
+    void onAltaModificacionClick(ActionEvent event) {
+        //Se recogen los datos de los campos y se crea un nuevo dispositivo
+
+        Dispositivo dispositivo;
+        if(tfMarca.getText().isEmpty() || tfModelo.getText().isEmpty() || tfPrecio.getText().isEmpty() || datePicker.getValue() == null || cbTipoDispositivo.getValue() == null) {
+            dispositivo = new Dispositivo();
+            dispositivo.setMarca(tfMarca.getText());
+            dispositivo.setModelo(tfModelo.getText());
+            dispositivo.setPrecio(Integer.parseInt(tfPrecio.getText()));
+            dispositivo.setFechaCompra(datePicker.getValue());
+            dispositivo.setTipoAtributo(cbTipoDispositivo.getValue());
+            if(dispositivo.getId() == null) {
+                dispositivo.setId(String.valueOf(inventarioController.getListaDispositivos().size() + 1));
+            }
+        } else {
+            //actualizar dispositivo que se ha seleccionado
+            dispositivo = inventarioController.getDispositivoSeleccionado();
+            dispositivo.setMarca(tfMarca.getText());
+            dispositivo.setModelo(tfModelo.getText());
+            dispositivo.setPrecio(Integer.parseInt(tfPrecio.getText()));
+            dispositivo.setFechaCompra(datePicker.getValue());
+            dispositivo.setTipoAtributo(cbTipoDispositivo.getValue());
+        }
+
+
+
+
+        if(!inventarioController.getListaDispositivos().contains(dispositivo)) {
+            inventarioController.addDispositivo(dispositivo);
+        } else{
+            inventarioController.updateDispositivo(dispositivo);
+        }
+
+        //se cierra la ventana
+        bt_Alta.getScene().getWindow().hide();
+    }
+
+    public void setDispositivo(Dispositivo dispositivo) {
+        tfMarca.setText(dispositivo.getMarca());
+        tfModelo.setText(dispositivo.getModelo());
+        tfPrecio.setText(String.valueOf(dispositivo.getPrecio()));
+        datePicker.setValue(dispositivo.getFechaCompra());
+        cbTipoDispositivo.setValue(dispositivo.getTipoAtributo());
+    }
+
+    public void setInventarioController(InventarioController inventarioController) {
+        this.inventarioController = inventarioController;
+    }
+
+
+
 
 }
--- src/main/java/org/example/actividadfinal/InventarioController.java
+++ src/main/java/org/example/actividadfinal/InventarioController.java
@@ -1,10 +1,17 @@
 package org.example.actividadfinal;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import org.example.actividadfinal.models.Dispositivo;
 
 public class InventarioController {
 
@@ -30,7 +37,21 @@ public class InventarioController {
     private Label labelTipoDispositivo;
 
     @FXML
-    private ListView<?> lvDispositivos;
+    private ListView<Dispositivo> lvDispositivos;
+
+    public InventarioController() {
+
+    }
+
+    @FXML
+    void onDispositivoClicked(MouseEvent event) {
+        Dispositivo dispositivo = lvDispositivos.getSelectionModel().getSelectedItem();
+        labelFecha.setText(dispositivo.getFechaCompra().toString());
+        labelMarca.setText(dispositivo.getMarca());
+        labelModelo.setText(dispositivo.getModelo());
+        labelPrecio.setText(String.valueOf(dispositivo.getPrecio()));
+        labelTipoDispositivo.setText(dispositivo.getTipoAtributo().toString());
+    }
 
     @FXML
     void onImprimirButtonClick(ActionEvent event) {
@@ -44,7 +65,46 @@ public class InventarioController {
 
     @FXML
     void ontAddEditClick(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("dispositivo-view.fxml"));
+            Parent root = fxmlLoader.load();
+
+            DispositivoController dispositivoController = fxmlLoader.getController();
+            dispositivoController.setInventarioController(this);
+            if (lvDispositivos.getSelectionModel().getSelectedItem() != null) {
+                dispositivoController.setDispositivo(lvDispositivos.getSelectionModel().getSelectedItem());
+            } else {
+                dispositivoController.setDispositivo(new Dispositivo());
+            }
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addDispositivo(Dispositivo dispositivo) {
+        lvDispositivos.getItems().add(dispositivo);
+    }
+
+    public void updateDispositivo(Dispositivo dispositivo) {
+        Dispositivo dispositivo1 =  lvDispositivos.getSelectionModel().getSelectedItem();
+        dispositivo1.setMarca(dispositivo.getMarca());
+        dispositivo1.setModelo(dispositivo.getModelo());
+        dispositivo1.setPrecio(dispositivo.getPrecio());
+        dispositivo1.setFechaCompra(dispositivo.getFechaCompra());
+        dispositivo1.setTipoAtributo(dispositivo.getTipoAtributo());
 
     }
 
+
+    public ObservableList<Dispositivo> getListaDispositivos() {
+        return lvDispositivos.getItems();
+    }
+
+    public Dispositivo getDispositivoSeleccionado() {
+        return lvDispositivos.getSelectionModel().getSelectedItem();
+    }
 }
